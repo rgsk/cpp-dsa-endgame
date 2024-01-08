3cd3e0dd52fc4d3c5bf6e0c3633d230740589b53
rgsk <guptarahul70322@gmail.com>
2024-01-08 13:13:10 +0530

    done

---
+++ src/codeforces/1325/A. EhAb AnD gCd/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t > 0) {
+        int v;
+        cin >> v;
+        cout << 1 << " " << v - 1 << endl;
+        t--;
+    }
+}
