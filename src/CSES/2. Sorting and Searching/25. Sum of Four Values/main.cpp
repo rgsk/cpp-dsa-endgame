af92fb9ab58228e275db5a1642ae5c5ae05935da
rgsk <guptarahul70322@gmail.com>
2024-05-27 02:31:53 +0530

    done

---
+++ src/CSES/2. Sorting and Searching/25. Sum of Four Values/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n, x;
+    cin >> n >> x;
+    vector<pair<int, int>> vec(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i].first;
+        vec[i].second = i + 1;
+    }
+    sort(vec.begin(), vec.end());
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int l = j + 1;
+            int r = n - 1;
+            while (l < r) {
+                int total = vec[i].first + vec[j].first + vec[l].first + vec[r].first;
+                if (total == x) {
+                    cout << vec[i].second << " " << vec[j].second << " " << vec[l].second << " " << vec[r].second << endl;
+                    return 0;
+                } else if (total < x) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+    }
+    cout << "IMPOSSIBLE" << endl;
+}
