57af7293b7b46222e34212072be75d005ecafc36
rgsk <guptarahul70322@gmail.com>
2024-05-28 04:24:13 +0530

    done

--- src/CSES/2. Sorting and Searching/33. Sliding Window Cost/main.cpp
+++ src/CSES/2. Sorting and Searching/33. Sliding Window Cost/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+typedef long long ll;
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -14,15 +14,21 @@ int main() {
     }
     multiset<int> maxHeap;
     multiset<int> minHeap;
+    ll minHeapSum = 0;
+    ll maxHeapSum = 0;
+
     for (int i = 0; i < k; i++) {
         maxHeap.insert(arr[i]);
+        maxHeapSum += arr[i];
     }
 
     auto erase = [&](int v) {
         if (v >= *minHeap.begin()) {
             minHeap.erase(minHeap.find(v));
+            minHeapSum -= v;
         } else {
             maxHeap.erase(maxHeap.find(v));
+            maxHeapSum -= v;
         }
     };
     auto balance = [&]() {
@@ -30,18 +36,24 @@ int main() {
             int v = *maxHeap.rbegin();
             minHeap.insert(v);
             maxHeap.erase(maxHeap.find(v));
+            minHeapSum += v;
+            maxHeapSum -= v;
         }
         while (minHeap.size() > maxHeap.size() + 2) {
             int v = *minHeap.begin();
             maxHeap.insert(v);
             minHeap.erase(minHeap.find(v));
+            maxHeapSum += v;
+            minHeapSum -= v;
         }
     };
     auto insert = [&](int v) {
         if (v >= *minHeap.begin()) {
             minHeap.insert(v);
+            minHeapSum += v;
         } else {
             maxHeap.insert(v);
+            maxHeapSum += v;
         }
     };
 
@@ -49,13 +61,21 @@ int main() {
         return *minHeap.begin();
     };
 
+    auto cost = [&]() {
+        int m = median();
+        // minHeap contains values greater than equal to median
+        ll updateMinHeapCost = minHeapSum - (m * minHeap.size());
+        ll updateMaxHeapCost = (m * maxHeap.size()) - maxHeapSum;
+        return updateMinHeapCost + updateMaxHeapCost;
+    };
+
     balance();
-    cout << median() << " ";
+    cout << cost() << " ";
     for (int i = k; i < n; i++) {
         insert(arr[i]);
         erase(arr[i - k]);
         balance();
-        cout << median() << " ";
+        cout << cost() << " ";
     }
     cout << endl;
 }
