8161473f7e274cc8bf7d66cace0b1f64403ea131
rgsk <guptarahul70322@gmail.com>
2024-05-28 19:05:27 +0530

    done

--- src/CSES/2. Sorting and Searching/14. Towers/main.cpp
+++ src/CSES/2. Sorting and Searching/14. Towers/main.cpp
@@ -8,13 +8,10 @@ int main() {
 #endif
     int n;
     cin >> n;
-    vector<int> values(n);
-    for (int i = 0; i < n; i++) {
-        cin >> values[i];
-    }
-    int ans = 0;
     multiset<int> st;
-    for (int v : values) {
+    for (int i = 0; i < n; i++) {
+        int v;
+        cin >> v;
         auto it = st.upper_bound(v);
         if (it != st.end()) {
             st.erase(it);
@@ -22,7 +19,6 @@ int main() {
         } else {
             st.insert(v);
         }
-        ans = max(ans, (int)st.size());
     }
-    cout << ans << endl;
+    cout << st.size() << endl;
 }
