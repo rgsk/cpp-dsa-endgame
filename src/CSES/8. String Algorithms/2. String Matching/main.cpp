6f48d4a8baebe19c36508ae58c3c0fdfaee16e7e
rgsk <guptarahul70322@gmail.com>
2024-06-02 17:12:33 +0530

    done

--- src/CSES/8. String Algorithms/2. String Matching/main.cpp
+++ src/CSES/8. String Algorithms/2. String Matching/main.cpp
@@ -1,9 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+// Function to create the "partial match" table (also known as "failure function") for the KMP algorithm
+std::vector<int> computeLPSArray(const std::string& pattern) {
+    int m = pattern.length();
+    std::vector<int> lps(m, 0);
+    int len = 0;
+    int i = 1;
+
+    while (i < m) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return lps;
+}
+
+// KMP search algorithm
+int KMPSearch(const std::string& pattern, const std::string& text) {
+    int m = pattern.length();
+    int n = text.length();
+
+    std::vector<int> lps = computeLPSArray(pattern);
+
+    int i = 0;  // index for text
+    int j = 0;  // index for pattern
+
+    int totalMatches = 0;
+
+    while (i < n) {
+        if (pattern[j] == text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j == m) {
+            // std::cout << "Found pattern at index " << i - j << std::endl;
+            totalMatches++;
+            j = lps[j - 1];
+        } else if (i < n && pattern[j] != text[i]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return totalMatches;
+}
 
 int main() {
 #ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
     freopen("input.txt", "r", stdin);
+
+    // Prlong longing the Output to output.txt file
     freopen("output.txt", "w", stdout);
 #endif
+    string text, pattern;
+    cin >> text >> pattern;
+    cout << KMPSearch(pattern, text) << endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<ll, ll> pii;
+
+// Function to create the "partial match" table (also known as "failure function") for the KMP algorithm
+std::vector<int> computeLPSArray(const std::string& pattern) {
+    int m = pattern.length();
+    std::vector<int> lps(m, 0);
+    int len = 0;
+    int i = 1;
+
+    while (i < m) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return lps;
+}
+
+// KMP search algorithm
+int KMPSearch(const std::string& pattern, const std::string& text) {
+    int m = pattern.length();
+    int n = text.length();
+
+    std::vector<int> lps = computeLPSArray(pattern);
+
+    int i = 0;  // index for text
+    int j = 0;  // index for pattern
+
+    int totalMatches = 0;
+
+    while (i < n) {
+        if (pattern[j] == text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j == m) {
+            // std::cout << "Found pattern at index " << i - j << std::endl;
+            totalMatches++;
+            j = lps[j - 1];
+        } else if (i < n && pattern[j] != text[i]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return totalMatches;
+}
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -11,34 +68,7 @@ int main() {
     // Prlong longing the Output to output.txt file
     freopen("output.txt", "w", stdout);
 #endif
-    ll n, m;
-    cin >> n >> m;
-    vector<vector<pair<ll, ll>>> graph(n + 1);
-    for (ll i = 0; i < m; i++) {
-        ll a, b, c;
-        cin >> a >> b >> c;
-        graph[a].push_back({c, b});
-    }
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    pq.push({0, 1});
-    vector<ll> distances(n + 1, LONG_MAX);
-    distances[1] = 0;
-    while (!pq.empty()) {
-        auto [dist, node] = pq.top();
-        pq.pop();
-        if (dist > distances[node]) {
-            continue;
-        }
-        for (auto [child_dist, child_node] : graph[node]) {
-            ll new_distance = dist + child_dist;
-            if (new_distance < distances[child_node]) {
-                distances[child_node] = new_distance;
-                pq.push({new_distance, child_node});
-            }
-        }
-    }
-    for (ll i = 1; i <= n; i++) {
-        cout << distances[i] << " ";
-    }
-    cout << endl;
+    string text, pattern;
+    cin >> text >> pattern;
+    cout << KMPSearch(pattern, text) << endl;
 }
