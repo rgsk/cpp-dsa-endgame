fadf189007d6694fc5f2d0d7c77ac4ca74e6dae5
rgsk <guptarahul70322@gmail.com>
2024-09-25 17:48:33 +0530

    done

---
+++ src/CSES/5. Range Queries/2. Static Range Minimum Queries/passed1.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SparseTable {
+   public:
+    vector<vector<int>> table;
+    vector<int> log_table;
+
+    SparseTable(const vector<int>& data) {
+        int n = data.size();
+        int max_log = log2(n) + 1;
+        table.resize(n, vector<int>(max_log));
+        log_table.resize(n + 1);
+
+        // Precompute log values
+        log_table[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            log_table[i] = log_table[i / 2] + 1;
+        }
+
+        // Build the sparse table
+        for (int i = 0; i < n; i++) {
+            table[i][0] = data[i];
+        }
+
+        for (int j = 1; j < max_log; j++) {
+            for (int i = 0; i + (1 << j) <= n; i++) {
+                table[i][j] = min(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+
+    int query(int l, int r) {
+        int log = log_table[r - l + 1];
+        return min(table[l][log], table[r - (1 << log) + 1][log]);
+    }
+};
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n, q;
+    cin >> n >> q;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    SparseTable st(nums);
+
+    for (int i = 0; i < q; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << st.query(a - 1, b - 1) << endl;  // Convert 1-based index to 0-based
+    }
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-ll dijkstra(ll n) {
-    // Min-heap (priority queue) to prioritize states with the smallest number of steps
-    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
-    set<ll> visited;  // To avoid reprocessing the same number
-
-    pq.push({n, 0});
-    visited.insert(n);
-
-    while (!pq.empty()) {
-        ll current = pq.top().first;
-        ll steps = pq.top().second;
-        pq.pop();
-
-        // If we have reached 0, return the number of steps
-        if (current == 0) {
-            return steps;
-        }
-
-        // Extract digits of the current number
-        ll temp = current;
-        while (temp > 0) {
-            ll digit = temp % 10;
-            temp /= 10;
-            if (digit > 0) {
-                ll next_num = current - digit;
-                // If the number has not been visited yet
-                if (visited.find(next_num) == visited.end()) {
-                    pq.push({next_num, steps + 1});  // Increment step count
-                    visited.insert(next_num);
-                }
+class Solution {
+   public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        // we need to find the first i where citations[i] >= n - i;
+        // and return n - i
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (citations[m] >= n - m) {
+                r = m;
+            } else {
+                l = m + 1;
             }
         }
+        return n - r;
     }
-
-    return -1;  // We should never reach here
-}
-
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    ll n;
-    cin >> n;
-
-    cout << dijkstra(n) << endl;
-
-    return 0;
-}
+};
